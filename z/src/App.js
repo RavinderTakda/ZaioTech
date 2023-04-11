1e230dcc321a5f36010e41fd64f52051d86672b2
Ravinder Saini <iamraviamar@gmail.com>
2023-04-11 14:26:57 +0530

    upload

---
+++ z/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Enroll } from './Components/Enrollpage';
+import { Rout} from './Components/Routes';
+import { SchedulePage } from './Components/Schedulepage';
+
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+<Rout/>
+    </div>
+  );
+}
+
+export default App;
